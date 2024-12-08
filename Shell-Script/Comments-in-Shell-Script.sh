3a7c04a90311c0ec8e6bc46746ba403f1077a140
Ayushraj021 <ayushraj02929@gmail.com>
2024-12-08 15:10:03 +0530

    Shell-Script

---
+++ Shell-Script/Comments-in-Shell-Script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Description: Comments-in-Shell-Script
+# Author:
+# Date Created:
+# Version: Single-line-comments
+# Date Modified:
+# Version: Multi-line-comments
+<< AYUSH 
+echo "Hello World"
+AYUSH
---
+++ Shell-Script/User-Input.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Description: Simple shell script to accept input from a user 
+# Author:
+# Date Created:
+# Version: 
+# Date Modified:
+echo "Hello Please enter your name: "
+read name
+echo "Please enter the course you're willing to learn"
+read course
+echo "Hello $name Welcome to the linux class, $name you are interested in $course course?"
+read verify
+echo "$verify ok!!"
